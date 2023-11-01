c8e2a47e22b5f1b4fdf7a0f15756e281c95b8c3f
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-11-01 17:47:03 -0500

    Update and rename placeholder to CreateTables.sql

---
+++ sql/CreateTables.sql
@@ -0,0 +1,44 @@
+DROP TABLE IF EXISTS CustomerData;
+DROP TABLE IF EXISTS LineItems;
+DROP TABLE IF EXISTS Quotes;
+DROP TABLE IF EXISTS SalesAssociate;
+
+CREATE TABLE CustomerData {
+  UserID int(15) UNIQUE PRIMARY KEY NOT NULL,
+  Name char(40) NOT NULL,
+  Email char(40) NOT NULL,
+  Country char(40) NOT NULL,
+  Address char(60) NOT NULL,
+  QuoteID int(15) NOT NULL,
+  FOREIGN KEY (QuoteID) REFRENCES Quotes(QuoteID)
+};
+
+CREATE TABLE LineItems {
+  LineID int(15) UNIQUE PRIMARY KEY NOT NULL,
+  ItemName char(40) NOT NULL,
+  Quantity int(3) NOT NULL,
+  UnitPrice double(5) NOT NULL,
+  Discount double(5),
+  TotalPrice double(7) NOT NULL,
+  QuoteID int(15) NOT NULL,
+  FOREIGN KEY (QuoteID) REFRENCES Quotes(QuoteID)
+}
+
+CREATE TABLE Quotes {
+  QuoteID int(15) UNIQUE PRIMARY KEY NOT NULL,
+  Date int(15) NOT NULL,
+  SecretNote char(256),
+  Status int(1) NOT NULL,
+}
+
+CREATE TABLE SalesAssociate {
+  AssocID int(15) UNIQUE PRIMARY KEY NOT NULL,
+  Name char(40) NOT NULL,
+  Email char(40) NOT NULL,
+  Address char(40) NOT NULL,
+  Username char(40) UNIQUE NOT NULL,
+  Password char(40) NOT NULL,
+  commission double(6) NOT NULL,
+  QuoteID int(15) NOT NULL,
+  FOREIGN KEY (QuoteID) REFRENCES Quotes(QuoteID)
+}
