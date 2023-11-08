ca495c13a6adcb9e4c4d021dac3ac18e49730d3e
OnSomeBread <77025944+OnSomeBread@users.noreply.github.com>
2023-11-08 16:26:26 -0600

    Update SystemAdmin.php

--- SystemAdmin.php
+++ SystemAdmin.php
@@ -5,13 +5,6 @@ include("pass+.php");
 echo "<head>
      </head>";
 echo "<body>";
-try {
-     $dsn = "mysql:host=courses;dbname={$dbname}";
-     $pdo = new PDO($dsn, $username, $password);
-     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  
-} catch(PDOexception $e) {
-     echo "Connection to database failed: " . $e->getMessage();
-}
+
 echo "</body>";
 ?>
