5020740e642178b6463d052f4a32655fb023ab6c
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-11-08 16:08:18 -0600

    Update SystemAdmin.php

--- php/SystemAdmin.php
+++ php/SystemAdmin.php
@@ -4,9 +4,12 @@ echo "<head>"
      "</head>";
 echo "<body>";
 try {
+     $dsn = "mysql:host=courses;dbname={$dbname}";
+     $pdo = new PDO($dsn, $username, $password);
+     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   
 } catch(PDOexception $e) {
-  echo "Connection to database failed: " . $e->getMessage();
+     echo "Connection to database failed: " . $e->getMessage();
 }
 echo "</body>";
 ?>
