1c741b14d99d4a1a752a5237f3a005eb8c27674f
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-11-08 15:43:12 -0600

    Update CreateTables.sql

--- sql/CreateTables.sql
+++ sql/CreateTables.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS SalesAssociate;
 
 CREATE TABLE Quotes (
   QuoteID int AUTO_INCREMENT PRIMARY KEY,
-  Date int(15) NOT NULL,
+  Time int(15) NOT NULL,
   SecretNote char(255),
   Status int(1) NOT NULL,
 );
