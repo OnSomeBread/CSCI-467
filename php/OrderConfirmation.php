8430078032c7ddb7b3e715f19f06f6af2ffff010
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-11-08 16:08:11 -0600

    Update OrderConfirmation.php

--- php/OrderConfirmation.php
+++ php/OrderConfirmation.php
@@ -1,12 +1,15 @@
 <?php
 
 echo "<head>"
-      "</head>";
+     "</head>";
 echo "<body>";
 try {
+     $dsn = "mysql:host=courses;dbname={$dbname}";
+     $pdo = new PDO($dsn, $username, $password);
+     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   
 } catch(PDOexception $e) {
-  echo "Connection to database failed: " . $e->getMessage();
+     echo "Connection to database failed: " . $e->getMessage();
 }
 echo "</body>";
 ?>
