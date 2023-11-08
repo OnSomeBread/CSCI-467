1f5fa01c778b1d3cb6e1a2d283b34175222dda25
OnSomeBread <77025944+OnSomeBread@users.noreply.github.com>
2023-11-08 16:25:43 -0600

    Update pass+.php

--- pass+.php
+++ pass+.php
@@ -5,4 +5,13 @@ $username = "z1927111";
 $password = "2002Sep16";
 //z-id
 $dbname   = "z1927111"; 
+
+try {
+     $dsn = "mysql:host=courses;dbname={$dbname}";
+     $pdo = new PDO($dsn, $username, $password);
+     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
+} catch(PDOexception $e) {
+     echo "Connection to database failed: " . $e->getMessage();
+}
 ?>
