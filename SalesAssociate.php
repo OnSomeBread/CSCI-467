d83f280c76c5e35fe0e171e0ac563e97c13afc4b
OnSomeBread <77025944+OnSomeBread@users.noreply.github.com>
2023-11-15 15:43:37 -0600

    Update SalesAssociate.php

--- SalesAssociate.php
+++ SalesAssociate.php
@@ -1,9 +1,9 @@
 <form action="" method="GET">
 	<br><br>
-	<h1>Please enter a username</h1>
+	<h3>Please enter a username</h1>
 	<input type="text" name="username" value="">
 	<br>
-	<h1>Please enter a password</h1>
+	<h3>Please enter a password</h1>
 	<input type="text" name="password" value="">
 	<br>
 	<input type="text" name="" value="">
