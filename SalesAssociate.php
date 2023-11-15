f2c158ecea26b935bfa0a6ab009921ff8de3da5e
OnSomeBread <77025944+OnSomeBread@users.noreply.github.com>
2023-11-15 16:07:42 -0600

    Update SalesAssociate.php

--- SalesAssociate.php
+++ SalesAssociate.php
@@ -18,5 +18,24 @@ echo "<head>
      </head>";
 echo "<body>";
 
+$query = $pdo->query("SELECT * FROM Quotes;");
+echo '<table>';
+	while($row = $query->fetch(PDO::FETCH_ASSOC)){
+                echo "<tr id=$i onclick='highlight(this)'>";
+                
+                // gets every col form result
+                $j = 0;
+                foreach($row as $col){
+                    $h = $i . $j . "d";
+                    echo "<td id=$h>";
+                    echo $col;
+                    echo "</td>";
+                    $j = $j + 1;
+                }
+                echo "</tr>";
+                $i = $i + 1;
+            }
+
+        echo '</table>';
 echo "</body>";
 ?>
