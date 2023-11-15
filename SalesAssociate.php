ae7f4a3342840da4acd7995c0e7238087a20177f
OnSomeBread <77025944+OnSomeBread@users.noreply.github.com>
2023-11-15 15:45:45 -0600

    Update SalesAssociate.php

--- SalesAssociate.php
+++ SalesAssociate.php
@@ -1,3 +1,4 @@
+<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fblog.hubspot.com%2Fblog%2Ftabid%2F6307%2Fbid%2F33356%2Fbrand-logos-the-good-the-bad-and-the-ugly.aspx&psig=AOvVaw1C5YXAEwNXLC0ZXXc3BNDm&ust=1700171094265000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMC2roP9xoIDFQAAAAAdAAAAABAE">
 <form action="" method="GET">
 	<br><br>
 	<h3>Please enter a username</h3>
