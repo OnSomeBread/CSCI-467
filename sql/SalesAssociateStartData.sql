a1526f96578e20a2ce067206501d71cbd6ac94bf
Muraad Khan <112736989+kmuraad@users.noreply.github.com>
2023-11-20 16:19:50 -0600

    Update SalesAssociateStartData.sql
    Created a Sales Associate account, v1.

--- sql/SalesAssociateStartData.sql
+++ sql/SalesAssociateStartData.sql
@@ -1,1 +1,7 @@
+-- Insert data into Quotes table
+INSERT INTO Quotes (Date_, SecretNote, Status) VALUES
+  (20230101, 'Confidential Note 1', 1);
 
+-- Insert data into SalesAssociate table
+INSERT INTO SalesAssociate (Name, Email, Address, Username, Password, commission, QuoteID) VALUES
+  ('John Doe', 'john.doe@example.com', '123 Main St', 'john_doe', 'password123', 5.0, 1);
