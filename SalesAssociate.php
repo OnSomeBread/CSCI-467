162418a0002d981488b4415d6f49ef35570a0bb9
OnSomeBread <77025944+OnSomeBread@users.noreply.github.com>
2023-11-20 16:11:52 -0600

    Update SalesAssociate.php

--- SalesAssociate.php
+++ SalesAssociate.php
@@ -29,15 +29,6 @@ th, td {
 
 
 <img src="">
-<form action="" method="GET">
-	<br><br>
-	<h3>Please enter a username</h3>
-	<input type="text" name="username" value="">
-	<h3>Please enter a password</h3>
-	<input type="text" name="password" value="">
-	<br>
-	<input type="text" name="" value="">
-</form>
 <?php
 include("pass+.php");
 
@@ -47,6 +38,20 @@ include("pass+.php");
 echo "<head>
      </head>";
 echo "<body>";
+
+if(!isset($_GET['login'])) {
+	echo '<form action="" method="GET">
+		<br><br>
+		<h3>Please enter a username</h3>
+		<input type="text" name="username" value="">
+		<h3>Please enter a password</h3>
+		<input type="text" name="password" value="">
+		<br>
+		<input type="text" name="" value="">
+		<br>
+		<button id="login" type="submit" name="login" value="login">Login</button
+	</form>';
+}
 
 $query = $pdo->query("SELECT * FROM Quotes;");
 echo '<table>';
