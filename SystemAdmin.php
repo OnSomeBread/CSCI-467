4720f477677705e9441da599129aa824264e9701
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-11-27 16:24:16 -0600

    Update SystemAdmin.php

--- SystemAdmin.php
+++ SystemAdmin.php
@@ -2,9 +2,29 @@
 
 include("pass+.php");
 
-echo "<head>
-     </head>";
+echo "<head><title>System Administration</title></head>";
 echo "<body>";
+echo '<form action="" method="POST">
+			<br><br>
+			<h3>Please enter a name</h3>
+			<input type="text" name="name">
+   
+			<h3>Please enter a email</h3>
+			<input type="text" name="email">
+
+               <h3>Please enter a address</h3>
+			<input type="text" name="address">
+
+               <h3>Please enter a username</h3>
+			<input type="text" name="username">
+
+               <h3>Please enter a password</h3>
+			<input type="text" name="password">
+
+			<br>
+			<button id="create" type="submit" name="create" value="create">Create</button
+		</form>';
+	echo "</body>";
 
 echo "</body>";
 ?>
