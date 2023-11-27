99cb109ce0b57505f85e0023e62a6e9d846c5efe
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-11-27 16:27:52 -0600

    Update SystemAdmin.php

--- SystemAdmin.php
+++ SystemAdmin.php
@@ -3,26 +3,29 @@
 include("pass+.php");
 
 echo "<head><title>System Administration</title></head>";
+
+if (isset($_GET['Name']) && isset($_GET['Email']) && isset($_GET['Country']) && isset($_GET['Address']) && isset($_GET['QuoteID'])){
+
 echo "<body>";
-echo '<form action="" method="POST">
-			<br><br>
-			<h3>Please enter a name</h3>
-			<input type="text" name="name">
+	echo '<form action="" method="POST">
+		<br><br>
+		<h3>Please enter a name</h3>
+		<input type="text" name="name">
    
-			<h3>Please enter a email</h3>
-			<input type="text" name="email">
+		<h3>Please enter a email</h3>
+		<input type="text" name="email">
 
-               <h3>Please enter a address</h3>
-			<input type="text" name="address">
+               	<h3>Please enter a address</h3>
+		<input type="text" name="address">
 
-               <h3>Please enter a username</h3>
-			<input type="text" name="username">
+               	<h3>Please enter a username</h3>
+		<input type="text" name="username">
 
-               <h3>Please enter a password</h3>
-			<input type="text" name="password">
+               	<h3>Please enter a password</h3>
+		<input type="text" name="password">
 
-			<br>
-			<button id="create" type="submit" name="create" value="create">Create</button
+		<br>
+		<button id="create" type="submit" name="create" value="create">Create</button
 		</form>';
 	echo "</body>";
 
