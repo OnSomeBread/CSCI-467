65a78cafffbbc5158efdf2e0bd441ca60ec6f4f2
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-11-29 15:38:53 -0600

    Update Management.php

--- Management.php
+++ Management.php
@@ -1,5 +1,7 @@
 <?php
-
+//Management: The overseers of finalized quotes, which they can reject or endorse. They manage line items, deal with prices and discounts,
+//and are a direct line of email communication with customers.
+//Use Cases = A lot of access, primary editors of project data. They can edit and manage most aspects of order, customer, discount admin. They can send email.
 include("pass+.php");
 
 echo "<head>
