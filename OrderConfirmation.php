91d2b73f8139638ce0dd1223cf6d453457cdc007
Muraad Khan <112736989+kmuraad@users.noreply.github.com>
2023-11-29 17:03:19 -0600

    Update OrderConfirmation.php

--- OrderConfirmation.php
+++ OrderConfirmation.php
@@ -17,31 +17,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
 $query = $pdo->query("SELECT * FROM Quotes WHERE Status = 2;");
-create_table_with_buttons($query);
+update_table_with_buttons($query);
 
 echo "</body>";
 
-function create_table_with_buttons($query)
-{
-    global $pdo;
 
-   echo "<form method='post' action='".$_SERVER["PHP_SELF"]."'>";
-
-   echo '<table>';
-    	while($row = $query->fetch(PDO::FETCH_ASSOC)){
-		echo "<tr>";
-	        	foreach($row as $col){
-	                    echo '<td style="padding: 10px; border: 1px solid #ddd;">';
-	                    	echo $col;
-	                    echo '</td>';
-	                }
-		echo "<td><button type='submit' name='quote_id' value='" . $row['QuoteID'] . "'>Update Status</button></td>";
-	        echo "</tr>";
-	}
-    echo '</table>';
-    
-    echo "</form>";
-}
 ?>
 
 
