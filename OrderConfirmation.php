7ca7f4ff40e8655c673d83ae80fba56443d5b431
Muraad Khan <112736989+kmuraad@users.noreply.github.com>
2023-11-29 16:42:56 -0600

    Update OrderConfirmation.php

--- OrderConfirmation.php
+++ OrderConfirmation.php
@@ -1,9 +1,4 @@
 <?php
-//Order Confirmation Department: The final group of company liaisons that manage the order, offer discounts,
-//review the order, forward the finalized quotes, and calculate the final price prior to shipment.
-//Use Cases = Send sectioned finalized quotes and add final discount to external processing system. Confirm data, in select cases: edit data. They can send emails.
-
-<?php
 include("pass+.php");
 
 echo "<head></head>";
@@ -50,4 +45,5 @@ function create_table_with_buttons($query)
     echo "</form>";
 }
 ?>
+
 
