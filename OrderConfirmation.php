b00a192e008e0f81ff3e7396d27b6cab685040fd
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-11-29 15:38:29 -0600

    Update OrderConfirmation.php

--- OrderConfirmation.php
+++ OrderConfirmation.php
@@ -1,4 +1,7 @@
 <?php
+//Order Confirmation Department: The final group of company liaisons that manage the order, offer discounts,
+//review the order, forward the finalized quotes, and calculate the final price prior to shipment.
+//Use Cases = Send sectioned finalized quotes and add final discount to external processing system. Confirm data, in select cases: edit data. They can send emails.
 
 include("pass+.php");
 
