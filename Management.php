aa2eca351bff5bb45a18b57c421dc557b6242a3e
Muraad Khan <112736989+kmuraad@users.noreply.github.com>
2023-11-29 15:56:29 -0600

    Update Management.php
    Added table

--- Management.php
+++ Management.php
@@ -8,8 +8,21 @@ echo "<head>
      </head>";
 echo "<body>";
 
+
      $query = $pdo->query("SELECT * FROM Quotes WHERE Status = 1;");
-     create_table($query);
+
+     echo '<table>';
+		while($row = $query->fetch(PDO::FETCH_ASSOC)){
+	                 echo '<tr>';
+    echo '<td style="padding: 10px; border: 1px solid #ddd;">' . $row['QuoteID'] . '</td>';
+    echo '<td style="padding: 10px; border: 1px solid #ddd;">' . $row['Date_'] . '</td>';
+    echo '<td style="padding: 10px; border: 1px solid #ddd;">' . $row['SecretNote'] . '</td>';
+    echo '<td style="padding: 10px; border: 1px solid #ddd;">' . $row['Status'] . '</td>';
+
+    echo '</tr>';
+	        }
+	echo '</table>';
+
 
 echo "</body>";
 ?>
