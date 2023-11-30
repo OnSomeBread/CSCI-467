13505704dfeaa8c2b97edfdbdbe55efbc34e53d4
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-11-29 18:18:47 -0600

    Update SalesAssociate.php

--- SalesAssociate.php
+++ SalesAssociate.php
@@ -53,7 +53,7 @@ echo "\n";
 
 echo "<form method=POST action=>";
 echo '<label for="customer">Select Customer:</label>'; 
-echo '<select id="customer" name="Name">';
+echo '<select id="customer" name="name">';
 echo '<option value="">select one</option>';
 foreach ($rows as $option){
 	echo "<option value=\"$option\">$option</option>";			  }
@@ -73,7 +73,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){ //check if form is submitted
 	//street -> Address
 	//Contact -> Email
     // Using prepared statement with placeholders
-    $n = $pdo->prepare("INSERT INTO CustomerData (Name, Email, Country, Address, QuoteID) VALUES (name, contact, city, street, :QuoteID)");
+    $n = $pdo->prepare("INSERT INTO CustomerData (Name, Email, Country, Address, QuoteID) VALUES (:Name, :Email, :Country, :Address, :QuoteID)");
     
     // Binding parameters
     $n->bindParam(':Name', $Name);
