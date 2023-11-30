5b7b4e3f431591c82381340165d36f88b3470981
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-11-29 19:06:40 -0600

    Update SalesAssociate.php

--- SalesAssociate.php
+++ SalesAssociate.php
@@ -62,8 +62,8 @@ echo "</form>";
 if ($_SERVER["REQUEST_METHOD"] == "POST"){ //check if form is submitted
 	$Name = $_POST["selected_customer"];
 					  
-	$xs = $pdx->query("SELECT name, street, city, contact FROM customers WHERE name = :Cname");
-	$xs->bindParam(':Cname', $Name);
+	$rs = $pdo->prepare("SELECT street, city, contact FROM customers WHERE name = :customer_name");
+	$rs->bindParam(':customer_name', $Name;
 	$xs->execute();
 	$xresult = $xs->fetch(PDO::FETCH_ASSOC);
     	$Email = $_POST["contact"];
