a7d37804d0539b20dca9b01407e72442e337a3b0
Rafael Diaz <77600422+z1873203@users.noreply.github.com>
2023-12-03 22:19:14 -0600

    Add files via upload

---
+++ main.php
@@ -0,0 +1,74 @@
+<html>
+<head>
+	<style>
+		body{
+			font-family: Arial, sans-serif;
+			background-color: #f4f4f4;
+			margin: 0;
+			padding: 0;
+			display: flex;
+            		flex-direction: column;
+            		align-items: left;
+			min-height: 100vh;
+		}
+
+		header {
+            		background-color: #2ecc71;
+            		color: #fff;
+            		padding: 10px;
+            		text-align: left;
+            		width: 100%;
+            		box-sizing: border-box;
+		}
+
+       		button {
+            		padding: 10px 20px;
+            		font-size: 16px;
+            		cursor: pointer;
+            		background-color: #3498db;
+            		color: #fff;
+            		border: none;
+            		border-radius: 5px;
+        	}
+
+        	button:hover {
+            		background-color: #2980b9;
+		}
+	</style>
+</head>
+<body>
+
+	<header>
+		<h1>Plant Repair Service</h1>
+	</header>	
+
+
+<?php
+	echo "<br>";			
+
+	echo "<h2>Sales Associate:</h2>";
+
+	echo '<a href="SalesAssociate.php"><button>Sales Associate</button></a>';
+
+	echo "<br>";
+
+	echo "<h2>Order Conformation page:</h2>";
+
+	echo '<a href="OrderConfirmation.php"><button>Order Conformation</button></a>';
+
+	echo "<br>";
+
+	echo "<h2>System Admin:</h2>";
+
+	echo '<a href="SystemAdmin.php"><button>System Admin</button></a>';
+
+	echo "<br>";
+
+	echo "<h2>Management:</h2>";
+
+	echo '<a href="Management.php"><button>Management</button></a>';
+
+	echo "<br>";
+?>
+</body>
+</html>
