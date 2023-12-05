faed9dc15fb9f1ae693781e874c3f36eb11050ce
OnSomeBread <77025944+OnSomeBread@users.noreply.github.com>
2023-12-05 11:41:43 -0600

    Update SalesAssociate.php

--- SalesAssociate.php
+++ SalesAssociate.php
@@ -69,7 +69,7 @@ if (isset($_POST['newQuote'])) {
     // Execute the SQL query
     	$g = $pdo->exec("INSERT INTO Quotes (Date_, SecretNote, Status) VALUES ('" . date("m/d/Y") . "', '', '0')");
 	$CurrentQID = $pdo->lastInsertID();
-	echo "Last Inserted ID: " . $CurrentQID;
+	echo "Your QuoteID is " . $CurrentQID;
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 try {
