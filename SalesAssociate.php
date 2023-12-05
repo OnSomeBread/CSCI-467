80ac62768bdf645b09402223a8c9dd32fbec2657
OnSomeBread <77025944+OnSomeBread@users.noreply.github.com>
2023-12-05 11:38:08 -0600

    Update SalesAssociate.php

--- SalesAssociate.php
+++ SalesAssociate.php
@@ -150,7 +150,7 @@ if($login == "correct"){
         $password = $_POST['password'];
 	echo '<input name="username" type="hidden" value=' . $username . '>';
 	echo '<input name="password" type="hidden" value=' . $password . '>';
-	echo '<input name="CurrentQID" type="hidden" ' . $CurrentQID . '>';
+	echo '<input name="CurrentQID" type="hidden" value=' . $CurrentQID . '>';
 	echo '<input name="newCust" type="submit" value="New Customer">';
 	echo '</select><br/>';	
 	echo "</form>";
