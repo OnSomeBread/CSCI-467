c731b30881c190c1f8bee760e0c74daed1968a93
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-12-05 16:08:47 -0600

    Update SalesAssociate.php

--- SalesAssociate.php
+++ SalesAssociate.php
@@ -265,7 +265,7 @@ if($login == "correct"){
 		echo '<input name="username" type="hidden" value=' . $username . '>';
 		echo '<input name="password" type="hidden" value=' . $password . '>';
 		echo '<input name="CurrentQID" type="hidden" value=' . $CurrentQID . '>';
-	    	echo '<a href="main.php"><input type="submit" name="finishQuote" value="Finish Quote">';
+	    	echo '<input type="submit" name="finishQuote" value="Finish Quote">';
 	echo '</form>';
 }
 ?>
