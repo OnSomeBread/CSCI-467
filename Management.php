2d204fae942dc7e8626bb1bb08a5976bfe9d4411
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2023-12-05 18:32:44 -0600

    Update Management.php

--- Management.php
+++ Management.php
@@ -9,8 +9,8 @@ echo "<head>
      </head>";
 echo "<body>";
 
-     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-         if (isset($_POST["quote_id"])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	if (isset($_POST["quote_id"])) {
              $quoteId = $_POST["quote_id"];
      
              // Update the "Status" to 3 for the selected QuoteID
@@ -18,16 +18,14 @@ echo "<body>";
              $updateQuery->bindParam(":quoteId", $quoteId, PDO::PARAM_INT);
              $updateQuery->execute();
          }
-     }
-if (isset($_GET['editNote'])){
-	$QuoteID = $_POST['QuoteID'];
-	$Message = $_POST['message'];
-	//set secret note on current QuoteID
-	$updateSecret = $pdo->prepare("UPDATE Quotes Set Secretnote = :message WHERE QuoteID = :QuoteID");
-	$updateSecret->bindParam(':message', $Message);
-	$updateSecret->bindParam(':quoteId', $QuoteID, PDO::PARAM_INT);
-	$updateSecret->execute();
-
+	if (isset($_GET['editNote'])){
+		$QuoteID = $_POST['QuoteID'];
+		$Message = $_POST['message'];
+		$updateSecret = $pdo->prepare("UPDATE Quotes Set SecretNote = :message WHERE QuoteID = :QuoteID");
+		$updateSecret->bindParam(':message', $Message);
+		$updateSecret->bindParam(':quoteId', $QuoteID, PDO::PARAM_INT);
+		$updateSecret->execute();
+	}
 }
 
 
@@ -41,14 +39,14 @@ if (isset($_GET['editNote'])){
           update_table_with_buttons($query);
      }
 
-          echo '<form method=POST action="">';
-			echo '<h3>Enter Quote ID to edit</h3>';
-			echo '<input type="text" name="QuoteID">';
+	echo '<form method=POST action="">';
+		echo '<h3>Enter Quote ID to edit</h3>';
+		echo '<input type="text" name="QuoteID">';
    
-   			echo '<label for="message">Message (up to 244 characters):</label>';
+   		echo '<label for="message">Message (up to 244 characters):</label>';
        		echo '<textarea id="message" name="message" rows="4" cols="50" maxlength="244" required></textarea>';
-			echo '<button id="edit" type="submit" name="editNote">Edit</button>';
-  		echo '</form>';
+		echo '<button id="edit" type="submit" name="editNote">Edit Quote</button>';
+  	echo '</form>';
 
 echo "</body>";
 ?>
