9efa5574bf8e92e29dc5136a5fc3e4b9a0ccbd71
Muraad Khan <112736989+kmuraad@users.noreply.github.com>
2023-12-06 15:31:21 -0600

    Update order.php
    Added function

--- order.php
+++ order.php
@@ -1,20 +1,24 @@
 <?php
-$url = 'http://blitz.cs.niu.edu/PurchaseOrder/';
-$data = array(
-	'order' => 'xyz-987654321-ba', 
-	'associate' => 'RE-676732',
-	'custid' => '21', 
-	'amount' => '7654.32');
-		
-$options = array(
-    'http' => array(
-        'header' => array('Content-type: application/json', 'Accept: application/json'),
-        'method'  => 'POST',
-        'content' => json_encode($data)
-    )
-);
+function sendPurchaseOrder($quoteID, $assocID, $custid, $priceTotal) {
+    $url = 'http://blitz.cs.niu.edu/PurchaseOrder/';
+    
+    $data = array(
+        'order' => $quoteID,
+        'associate' => $assocID,
+        'custid' => $custid,
+        'amount' => $priceTotal
+    );
 
-$context  = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
-echo($result);
-?>
+    $options = array(
+        'http' => array(
+            'header' => array('Content-type: application/json', 'Accept: application/json'),
+            'method'  => 'POST',
+            'content' => json_encode($data)
+        )
+    );
+
+   	$context  = stream_context_create($options);
+	$result = file_get_contents($url, false, $context);
+	echo($result);
+}
+<?php
